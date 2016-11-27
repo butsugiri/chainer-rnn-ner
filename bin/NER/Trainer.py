cec332a9b50aaab1eac75f710a6db1d2c42697cb
Shun Kiyono <kiyono@ecei.tohoku.ac.jp>
2016-11-27 12:39:37 +0900

    add

--- bin/NER/Trainer.py
+++ bin/NER/Trainer.py
@@ -4,15 +4,50 @@ import chainer
 import json
 import chainer.links as L
 import chainer.serializers as S
+import chainer.functions as F
 import numpy as np
 from chainer import cuda
 from .Model import NERTagger
 from .DataProcessor import DataProcessor
 from datetime import datetime
 
-class Classifier(chainer.link.Chain):
-    pass
 
+class Classifier(chainer.link.Chain):
+    compute_accuracy = True
+
+    def __init__(self, predictor, lossfun=F.softmax_cross_entropy,
+                 accfun=F.accuracy):
+        super(Classifier, self).__init__(predictor=predictor)
+        self.lossfun = lossfun
+        self.accfun = accfun
+        self.y = None
+        self.loss = None
+        self.accuracy = None
+
+    def __call__(self, *args, train=True):
+        assert len(args) >= 2
+        x = args[:-1]
+        t = args[-1]
+        self.y = None
+        self.loss = None
+        self.accuracy = None
+        self.y = self.predictor(*x, train)
+        for yi, ti, in zip(self.y, t):
+            if self.loss is not None:
+                self.loss += self.lossfun(yi, ti)
+            else:
+                self.loss = self.lossfun(yi, ti)
+
+        count = 0
+        if self.compute_accuracy:
+            for yi, ti in zip(self.y, t):
+                if self.accuracy is not None:
+                    self.accuracy += self.accfun(yi, ti) * len(ti)
+                    count += len(ti)
+                else:
+                    self.accuracy = self.accfun(yi, ti) * len(ti)
+                    count += len(ti)
+        return self.loss, self.accuracy, count
 
 class Trainer(object):
 
@@ -31,11 +66,9 @@ class Trainer(object):
             "time_taken": [], "perplexity": []}
         self.result_storage["hyper_params"] = config
 
-        # self.model = L.Classifier(
-        #     NERTagger(vocab=self.data_processor.vocab, embed_dim=config["embed_dim"]))
-        self.model = NERTagger(n_vocab=len(self.data_processor.vocab), embed_dim=config["embed_dim"],
-        hidden_dim=config["hidden_dim"], n_tag=len(self.data_processor.tag))
-        self.model.compute_accuracy = False
+        tagger = NERTagger(n_vocab=len(self.data_processor.vocab), embed_dim=config["embed_dim"],
+                           hidden_dim=config["hidden_dim"], n_tag=len(self.data_processor.tag))
+        self.model = Classifier(tagger)
         if self.use_gpu >= 0:
             chainer.cuda.get_device(self.use_gpu).use()  # make the GPU current
             self.model.to_gpu()
@@ -50,68 +83,48 @@ class Trainer(object):
 
     def run(self):
         for epoch in range(self.epoch):
+            sum_loss = 0
+            sum_acc = 0
+            sum_count = 0
             sys.stderr.write("Currently @ Epoch:{epoch}\n".format(epoch=epoch))
             self.epoch_start_time = datetime.now()
             for xs, ts in self.data_processor.batch_iter():
                 # print(xs)
                 # print(ts)
                 hx = chainer.Variable(
-                self.xp.zeros((1, len(xs), self.config["hidden_dim"]), dtype=self.xp.float32))
+                    self.xp.zeros((1, len(xs), self.config["hidden_dim"]), dtype=self.xp.float32))
                 cx = chainer.Variable(
-                self.xp.zeros((1, len(xs), self.config["hidden_dim"]), dtype=self.xp.float32))
-                y = self.model(xs, hx, cx)
-
-                exit()
-
-
-
-
-
-
-                for curr_words, next_words in zip(batch.T, batch[:, 1:].T):
-                    accum_loss += self.model(curr_words, next_words)
+                    self.xp.zeros((1, len(xs), self.config["hidden_dim"]), dtype=self.xp.float32))
+                loss, acc, count = self.model(xs, hx, cx, ts)
+                sum_loss += loss.data
+                sum_acc += acc.data
+                sum_count += count
                 self.optimizer.target.cleargrads()
-                accum_loss.backward()
+                loss.backward()
                 self.optimizer.update()
-                self.model.predictor.reset_state()
-            self.epoch_end_time = datetime.now()
 
-            if epoch + 1 % 50 == 0:
-                self._take_snapshot(epoch)
+            print("{}\t{}\t{}".format(sum_acc/sum_count, sum_loss, sum_count))
+            if (epoch + 1) % 10 == 0:
                 self._evaluate(epoch)
 
+
     def _evaluate(self, epoch):
-        accum_loss = 0
-        self.model.predictor.reset_state()
+        for xs, ts in self.data_processor.batch_iter(train=True):
+            hx = chainer.Variable(
+                self.xp.zeros((1, len(xs), self.config["hidden_dim"]), dtype=self.xp.float32))
+            cx = chainer.Variable(
+                self.xp.zeros((1, len(xs), self.config["hidden_dim"]), dtype=self.xp.float32))
+            ys = self.model.predictor(xs, hx, cx, train=False)
+            for yi, xi, ti in zip(ys, xs, ts):
+                pred = F.argmax(F.softmax(yi), axis=1).data
+                pred_tags = [self.data_processor.id2tag[i] for i in pred]
+                print(" ".join(pred_tags))
 
-        dataset_count = 0
-        for batch in self.data_processor.batch_iter(train=self.config["cheat"], batch_size=self.config["batch_size"], shuffle_batch=False):
-            for curr_words, next_words in zip(batch.T, batch[:, 1:].T):
-                accum_loss += cuda.to_cpu(self.model(curr_words,
-                                                     next_words).data)
-                dataset_count += 1
-            self.model.predictor.reset_state()
+                targets = [self.data_processor.id2tag[i] for i in ti]
+                print(" ".join(targets))
 
-        perp = np.exp(float(accum_loss) / (dataset_count * self.config["batch_size"]))
-
-        # epoch単位の実行時間の計算
-        td = self.epoch_end_time - self.epoch_start_time  # td: timedelta
-        hours, remainder = divmod(td.seconds, 3600)
-        minutes, seconds = divmod(remainder, 60)
-        time_taken = "{hours:02d}:{minutes:02d}".format(
-            hours=hours, minutes=minutes)
-
-        # epoch単位の各種statsの出力
-        sys.stderr.write("Epoch: #{epoch}\tTotal Loss:{total:.4f}\tTime Taken:{time_taken}\tPerplexity:{perp}\n".format(
-            epoch=epoch,
-            total=accum_loss,
-            time_taken=time_taken,
-            perp=perp
-        ))
-
-        self.result_storage["result"]["time_taken"].append(time_taken)
-        self.result_storage["result"]["perplexity"].append(perp)
-        self._save_stats()
+                # terms = [self.data_processor.id2vocab[i] for i in xi]
+                # print(" ".join(terms))
 
     def _save_stats(self):
         """
