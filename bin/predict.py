4688001686fc751ea0cf9c4fb4dc62907cd68cd4
Shun Kiyono <kiyono@ecei.tohoku.ac.jp>
2016-11-29 15:58:12 +0900

    now outputs score

--- bin/predict.py
+++ bin/predict.py
@@ -4,6 +4,8 @@
 NERタグの予測を出力するスクリプト
 """
 import chainer
+import numpy as xp
+import chainer.functions as F
 from chainer import training
 from chainer import serializers
 from train_model import Classifier, MyUpdater
@@ -23,12 +25,33 @@ def main():
     ))
     optimizer = chainer.optimizers.Adam()
     optimizer.setup(model)
-    train = data.train_data
-    dev = data.dev_data
 
-    train_iter = chainer.iterators.SerialIterator(train, batch_size=10)
-    for batch in train_iter:
-        print(batch)
+    test = data.test_data
+    serializers.load_npz("./result/model_iter_4213", model)
+
+    test_iter = chainer.iterators.SerialIterator(test, repeat=False, batch_size=10)
+
+    id2tag = data.id2tag
+    id2vocab = data.id2vocab
+    for batch in test_iter:
+        xs = [xp.array(x[0], dtype=xp.int32) for x in batch]
+        ts = [xp.array(x[1], dtype=xp.int32) for x in batch]
+        hx = chainer.Variable(
+            xp.zeros((1, len(xs), 100), dtype=xp.float32))
+        cx = chainer.Variable(
+            xp.zeros((1, len(xs), 100), dtype=xp.float32))
+        ys = model.predictor(xs, hx, cx, train=False)
+
+        for y, t in zip(ys, ts):
+            pred_ids = F.argmax(F.softmax(y), axis=1).data
+            pred_labels = [id2tag[x] for x in pred_ids]
+
+            target_labels = [id2tag[x] for x in t]
+
+            for predict, target in zip(pred_labels, target_labels):
+                print("{}\t{}".format(predict, target))
+
+
 
 if __name__ == '__main__':
     main()
