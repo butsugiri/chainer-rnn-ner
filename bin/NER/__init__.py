e226df74ed8716b57d1f39334f47a579417da523
Shun Kiyono <kiyono@ecei.tohoku.ac.jp>
2016-12-30 20:31:19 +0900

    fix

--- bin/NER/__init__.py
+++ bin/NER/__init__.py
@@ -1,4 +1,4 @@
 from .DataProcessor import DataProcessor
 from .Model import NERTagger, BiNERTagger, BiCharNERTagger
-from .Resource import Resource
 from .CRFModel import CRFNERTagger, CRFBiNERTagger, CRFBiCharNERTagger
+from .Resource import Resource
